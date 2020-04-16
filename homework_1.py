1bb962d2ddda4c1be440dddb2e64749e2bc66aa6
Boris Kaiser <boris@kayzer.ru>
2020-04-16 20:33:05 +0300

    age and year should be checked if they are digits

--- homework_1.py
+++ homework_1.py
@@ -17,7 +17,7 @@ while not next_year:
     next_year = input("Enter the year, you whant to calculate your age (greater than 2020): ")
     if not re.search(r"202[1-9]|20[1-9][0-9]|21[0-9]{2}", next_year):
         next_year = None
-        print("Sorry, but age should be greater than 2020")
+        print("Sorry, but year should be greater than 2020")
 
 future_age = str(int(next_year) - 2020 + int(age))
 print(f'Dear %USERNAME%, your age in {next_year} is {future_age}')
