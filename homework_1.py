5f015045e7c49baa47ce257fa6ec9b9b32f648a7
Boris Kaiser <boris@kayzer.ru>
2020-04-16 20:28:08 +0300

    age and year should be checked if they are digits

--- homework_1.py
+++ homework_1.py
@@ -1,6 +1,21 @@
 # calculate the user's age
-age = input("Enter your current age: ")
-next_year = input("Enter the year, you whant to calculate your age: ")
+import re
+
+age = None
+next_year = None
 cur_year = 2020
+
+while not age:
+    age = input("Enter your current age (please type only digits): ")
+    if not re.search(r"^[0-9]+$", age):
+        print("Try again, and enter only digits")
+        age = None
+
+while not next_year:
+    next_year = input("Enter the year, you whant to calculate your age (greater than 2020): ")
+    if not re.search(r"202[1-9]|20[1-9][0-9]|21[0-9]{2}", next_year):
+        next_year = None
+        print("Sorry, but age should be greater than 2020")
+
 future_age = str(int(next_year) - 2020 + int(age))
 print(f'Dear %USERNAME%, your age in {next_year} is {future_age}')
